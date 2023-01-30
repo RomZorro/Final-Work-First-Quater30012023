514d3dc04bc27d572824b31872d22c8d5a6c4837
roman zotov <mr_monkey@inbox.ru>
2023-01-30 12:18:26 +0300

    Third part

--- 1st Project/Program.cs
+++ 1st Project/Program.cs
@@ -12,5 +12,13 @@ class Program
             Console.Write("Введите строку {0}: ", i);
             arr[i] = Console.ReadLine();
         }
+        // Вывод массивов длинна которых меньше или равна 3 символам
+        Console.Write("\nМассивы длинна которых меньше или равна 3 символам: ");
+        foreach (string s in arr)
+        {
+            if (s.Length <= 3)
+                Console.Write("{0} ", s );
+        }
+        Console.WriteLine();
     }
 }
