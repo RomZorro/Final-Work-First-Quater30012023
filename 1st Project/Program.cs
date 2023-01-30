62af8334b58284346d4257cf947bbc64a18271da
roman zotov <mr_monkey@inbox.ru>
2023-01-30 12:16:35 +0300

    Second part enter the elements

--- 1st Project/Program.cs
+++ 1st Project/Program.cs
@@ -5,6 +5,12 @@ class Program
         // Ввод количества элементов массива с клавиатуры
         Console.Write("Введите количество элементов массива: ");
         int n = int.Parse(Console.ReadLine());
-
+        // Ввод элементов массива с клавиатуры
+        string[] arr = new string[n];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("Введите строку {0}: ", i);
+            arr[i] = Console.ReadLine();
+        }
     }
 }
